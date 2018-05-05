0eae01834b782a5ff08a2bc5b8b166fa2ad20630
Krittiya Clark <krittiyaclark@gmail.com>
2018-05-05 18:43:30 -0400

    Create grid function work

--- designs.js
+++ designs.js
@@ -1,11 +1,26 @@
 // Select color input
 const colorPicker = document.querySelector('#colorPicker');
-// Select size input
-const inputHeight = document.querySelector('#inputHeight').value;
-const inputWidth = document.querySelector('#inputWidth').value;
+// Submit button
+let submitButton = document.querySelector('#submitButton');
+// Grid
+let grid = document.querySelector('#pixelCanvas');
 // When size is submitted by the user, call makeGrid()
-makeGrid();
+submitButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  makeGrid()
+  console.log('Work!');
+}); 
 
 function makeGrid() {
-  // Your code goes here!
+  // Select size input
+  let inputHeight = document.querySelector('#inputHeight').value;
+  let inputWidth = document.querySelector('#inputWidth').value;
+  for (let row = 1; row <= inputHeight; row++) {
+    let makeRow = document.createElement('tr')
+    grid.appendChild(makeRow);
+    for (let col = 1; col <= inputWidth; col++) {
+      let makeCol = document.createElement('td');
+      makeRow.appendChild(makeCol);
+    }
+  }
 }
