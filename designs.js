776fff72822390098d75194d224fde7f0ec44008
Krittiya Clark <krittiyaclark@gmail.com>
2018-05-05 17:39:37 -0400

    Add code starter

---
+++ designs.js
@@ -0,0 +1,11 @@
+// Select color input
+const colorPicker = document.querySelector('#colorPicker');
+// Select size input
+const inputHeight = document.querySelector('#inputHeight').value;
+const inputWidth = document.querySelector('#inputWidth').value;
+// When size is submitted by the user, call makeGrid()
+makeGrid();
+
+function makeGrid() {
+  // Your code goes here!
+}
